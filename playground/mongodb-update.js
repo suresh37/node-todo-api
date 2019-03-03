16ced77f80b9a613e49fe1ccad85279ec3468b58
suresh37 <sureshkarthick37@gmail.com>
2019-03-03 16:22:42 +0530

    added mongo delete script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,17 @@
+const { MongoClient, ObjectID } = require('mongodb');
+const url = 'mongodb://localhost:27017/';
+const dbName = 'TodoApp'; const col = 'Todos';
+MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    if (err)
+        return console.log("Error: " + err);
+    console.log('successfully connected to mongodb...');
+    var db = client.db(dbName);
+    db.collection(col).findOneAndUpdate({ _id: new ObjectID('5c7b642bf41c84711826a44e') },
+        {
+            $set: {
+                completed: true
+            }
+        },
+        { returnOriginal: false })
+        .then((res) => console.log(res))
+})
